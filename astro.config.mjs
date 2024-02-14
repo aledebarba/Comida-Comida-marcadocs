96f5d8c8c9387dc9c1731ca769e7cc688797c2e0
Alexandre CMC Souza <ale@alemacedo.com>
2024-02-14 07:22:13 -0300

    Deploy setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import starlight from "@astrojs/starlight";
 import react from "@astrojs/react";
 
 export default defineConfig({
+  site: "https://aledebarba.github.io",
+  base: "/Comida-Comida-marcadocs",
   integrations: [
     astroExpressiveCode(),
     starlight({
