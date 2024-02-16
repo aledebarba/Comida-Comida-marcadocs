e7cd1e8a2b33f203645f41393df05a09cbaf88fc
Alexandre CMC Souza <ale@alemacedo.com>
2024-02-16 16:30:14 -0300

    Lista de posts atualizados na home page.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,78 +4,110 @@ import mdx from "@astrojs/mdx";
 import starlight from "@astrojs/starlight";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
+import { remarkModifiedTime } from "./remark-modified-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://comida2xmarca.vercel.app',
-  integrations: [astroExpressiveCode(), starlight({
-    title: "A marca Comida-Comida.",
-    defaultLocale: "pt-BR",
-    locales: {
-      root: {
-        label: "Português do Brasil",
-        dir: "ltr",
-        lang: "pt-BR"
-      }
+    site: "https://comida2xmarca.vercel.app",
+    markdown: {
+      remarkPlugins: [remarkModifiedTime],
+      include: ["src/content/docs/**/*.mdx","src/**/*.mdx"],
     },
-    social: {
-      github: "https://github.com/aledebarba",
-      twitter: "https://twitter.com/alemacedo"
-    },
-    sidebar: [{
-      label: "Documentação de implementação da marca",
-      items: [
-      // Each item here is one entry in the navigation menu.
-      {
-        label: "Conteúdo",
-        link: "/brand/content/"
-      }, {
-        label: "Briefing",
-        link: "/brand/briefing/"
-      }, {
-        label: "Contexto e Moodboard",
-        link: "/brand/idv/"
-      }, {
-        label: "Logotipo e outros elementos de comunicação visual",
-        link: "/brand/brand/"
-      }, {
-        label: "Manual de uso",
-        link: "/brand/design-guide/"
-      }, {
-        label: "Aplicação de Cor",
-        link: "/brand/design-guide-paleta/"
-      }, {
-        label: "Personagens",
-        link: "/brand/design-guide-mascotes/"
-      }, {
-        label: "Mockups",
-        link: "/brand/design-guide-aplicacoes/"
-      }, {
-        label: "Material de Impressão",
-        link: "/brand/print/"
-      }, {
-        label: "Redes Sociais",
-        link: "/brand/social/"
-      }, {
-        label: "Design Kit",
-        link: "/brand/design-kit/"
-      }, {
-        label: "Press Kit",
-        link: "/brand/press-kit/"
-      }, {
-        label: "Sobre",
-        link: "/brand/about/"
-      }]
-    }, {
-      label: "Referências:",
-      autogenerate: {
-        directory: "reference"
-      }
-    }],
-    customCss: [
-    // Caminho relativo ao seu arquivo CSS customizado
-    './src/styles/custom.css']
-  }), mdx(), react({
-    include: ['**/react/*']
-  }), sitemap()]
+    integrations: [
+        astroExpressiveCode(),
+        mdx( {
+          remarkPlugins: [remarkModifiedTime],
+          include: ["src/content/docs/**/*.mdx","src/**/*.mdx"],
+      } ),
+        starlight({
+            title: "A marca Comida-Comida.",
+            defaultLocale: "pt-BR",
+            locales: {
+                root: {
+                    label: "Português do Brasil",
+                    dir: "ltr",
+                    lang: "pt-BR",
+                },
+            },
+            lastUpdated: true,
+            social: {
+                github: "https://github.com/aledebarba",
+                twitter: "https://twitter.com/alemacedo",
+            },
+            sidebar: [
+                {
+                    label: "Documentação de implementação da marca",
+                    items: [
+                        // Each item here is one entry in the navigation menu.
+                        {
+                            label: "Conteúdo",
+                            link: "/brand/content/",
+                        },
+                        {
+                            label: "Briefing",
+                            link: "/brand/briefing/",
+                        },
+                        {
+                            label: "Contexto e Moodboard",
+                            link: "/brand/idv/",
+                        },
+                        {
+                            label: "Logotipo e outros elementos de comunicação visual",
+                            link: "/brand/brand/",
+                        },
+                        {
+                            label: "Manual de uso",
+                            link: "/brand/design-guide/",
+                        },
+                        {
+                            label: "Aplicação de Cor",
+                            link: "/brand/design-guide-paleta/",
+                        },
+                        {
+                            label: "Personagens",
+                            link: "/brand/design-guide-mascotes/",
+                        },
+                        {
+                            label: "Mockups",
+                            link: "/brand/design-guide-aplicacoes/",
+                        },
+                        {
+                            label: "Material de Impressão",
+                            link: "/brand/print/",
+                        },
+                        {
+                            label: "Redes Sociais",
+                            link: "/brand/social/",
+                        },
+                        {
+                            label: "Design Kit",
+                            link: "/brand/design-kit/",
+                        },
+                        {
+                            label: "Press Kit",
+                            link: "/brand/press-kit/",
+                        },
+                        {
+                            label: "Sobre",
+                            link: "/brand/about/",
+                        },
+                    ],
+                },
+                {
+                    label: "Referências:",
+                    autogenerate: {
+                        directory: "reference",
+                    },
+                },
+            ],
+            customCss: [
+                // Caminho relativo ao seu arquivo CSS customizado
+                "./src/styles/custom.css",
+            ],
+        }),
+        react({
+            include: ["**/react/*"],
+        }),
+        sitemap(),
+    ],
 });
